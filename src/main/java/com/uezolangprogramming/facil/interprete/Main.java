0c9ba135ee0dcc40d178a65b148ee7db882102e9
Max <max.phazen@gmail.com>
2018-05-18 21:55:43 -0300

    Mensagens do console em portugues!

--- src/main/java/com/uezolangprogramming/facil/interprete/Main.java
+++ src/main/java/com/uezolangprogramming/facil/interprete/Main.java
@@ -1,5 +1,6 @@
 
 package com.uezolangprogramming.facil.interprete;
+
 import java.io.IOException;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
@@ -10,20 +11,25 @@ public class Main {
 	private static final String EXTENSION = "fcl";
 
 	public static void main(String[] args) throws IOException {
-		String program = args.length > 1 ? args[1] : "test/test." + EXTENSION;
+		String programa = args.length > 1 ? args[1] : "test/test." + EXTENSION;
 
-		System.out.println("Interpreting file " + program);
+		System.out.println("Interpretando Arquivo -> " + programa);
 
-		FacilLexer lexer = new FacilLexer(new ANTLRFileStream(program));
+		// Analisador Lexico
+		FacilLexer lexer = new FacilLexer(new ANTLRFileStream(programa));
+		// Saida do Analisador Lexico sendo preparada para o sintatico
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		// Analisador Sintatico
 		FacilParser parser = new FacilParser(tokens);
 
+		// Executa Analisador Sintatico para gerar uma arvode de sintaxe
 		FacilParser.StartContext tree = parser.start();
 
+		// Cria um visitante para visitar cada nodulo e realizar a analise semantica
 		FacilCustomVisitor visitor = new FacilCustomVisitor();
 		visitor.visit(tree);
 
-		System.out.println("Interpretation finished");
+		System.out.println("Final da Interpretacao");
 
 	}
 
